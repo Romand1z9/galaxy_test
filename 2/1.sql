7f1fe101d7388f2bac6d9a053ea31e293d27ff41
Romand1z9 <jamkowoyd1z9@gmail.com>
2020-08-17 12:02:43 +0700

    Some changes.

--- 2/1.sql
+++ 2/1.sql
@@ -8,5 +8,5 @@
 SELECT u.name as username, c.name as community_name, cm.joined_at as user_joined_at FROM testing.users as u
 INNER JOIN testing.community_members as cm ON u.id = cm.user_id
 INNER JOIN testing.communities as c ON cm.community_id = c.id
-WHERE c.created_at > "2013-01-01 00:00:00"
+WHERE c.created_at >= "2013-01-01 00:00:00"
 ORDER BY cm.joined_at DESC
